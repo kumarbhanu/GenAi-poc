e1914a38bb1d5298f87fdd48d7a4ca010d471da3
kumarbhanu <50135549+kumarbhanu@users.noreply.github.com>
2025-01-23 21:51:41 +0530

    Update no.py

--- final/no.py
+++ final/no.py
@@ -359,9 +359,7 @@ from langchain.document_loaders import TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 import streamlit as st
 
-file_path = "eds_data.txt"
-loader = TextLoader(file_path, encoding="utf-8")
-documents = loader.load()
+
 
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
 docs = text_splitter.split_documents(documents)
